885749d8456cdfadf899aaa1542e2452459b00a0
HYJ <hyjinny00@gmail.com>
2021-02-15 02:16:08 +0900

    JS: await

--- async/async.js
+++ async/async.js
@@ -1,6 +1,15 @@
 // 1. async
+
+// promise
+// function fetchUser(){
+//     return new Promise((resolve, rejcet) => { // promise 생성자 생성
+//         resolve('ellie');
+//     });
+// }
+
+// async
 async function fetchUser(){
-    return '하읭';
+    return 'ellie'; // 생성자 생성 필요 X
 }
 
 const user = fetchUser();
@@ -9,4 +18,51 @@ console.log(user);
 
 // 2. await
 
-// 3. useful Promise APIs
+function delay(ms){
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function getApple(){
+    await delay(1000); // 1초
+    return '🍎';
+}
+
+async function getBanana(){
+    await delay(1000); // 2초
+    return '🍌';
+}
+
+// promise
+// function pickFruits(){
+//     return getApple().then((apple) => {
+//         return getBanana().then((banana) => {
+//                 `${apple} + ${banana}`
+//         });
+//     });
+// }
+
+// await
+async function pickFruits(){
+    const applePromise = getApple();
+    const bananaPromise = getBanana();
+    const apple = await applePromise;
+    const banana = await bananaPromise;
+    return `${apple} + ${banana}`;
+}
+
+pickFruits().then(console.log);
+
+// 3. Promise APIs
+
+function pickAllFruits(){
+    return Promise.all([getApple(), getBanana()]) // 배열 모두 출력
+        .then((fruits) => fruits.join(' + '));
+}
+pickAllFruits().then(console.log);
+
+function pickOnlyOne(){
+    return Promise.race([getApple(), getBanana()]); // 배열 중 가장 먼저 실행되는거 출력
+}
+pickOnlyOne().then(console.log);
